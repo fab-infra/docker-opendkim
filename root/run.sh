02f3709b2d34a2ab0bb3624c73cfb7da0da13d6f
Fabien Crespel <fabien@crespel.net>
2020-10-18 22:44:24 +0200

    Add chmod for keyfile directory

--- root/run.sh
+++ root/run.sh
@@ -12,6 +12,7 @@ confd -onetime -backend env
 # Check permissions
 if [ -e "$DKIM_KEYFILE" ]; then
     chmod 600 "$DKIM_KEYFILE" || true
+    chmod 700 `dirname "$DKIM_KEYFILE"` || true
 else
     echo "ERROR: key file '$DKIM_KEYFILE' is missing or not accessible"
     exit 1
