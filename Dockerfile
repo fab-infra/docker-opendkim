685bd4494672fb53fe9c2d873c5ab3eeec0dfca0
Fabien Crespel <fabien@crespel.net>
2022-03-20 22:47:31 +0100

    Fix indentation, move env vars

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,22 @@
 # OpenDKIM server based on openSUSE Leap 15.3
 FROM ghcr.io/fab-infra/base-image:opensuse15.3
 
-# MariaDB
+# Environment
+ENV DKIM_DOMAIN="localdomain" \
+	DKIM_KEYFILE="/var/lib/opendkim/private.key" \
+	DKIM_SELECTOR="default" \
+	DKIM_INTERNALHOSTS="127.0.0.1"
+
+# OpenDKIM
 RUN zypper in -y opendkim &&\
-    zypper clean -a
+	zypper clean -a
 
 # Files
 COPY ./root /
 RUN cp -p /etc/opendkim/opendkim.conf /etc/opendkim/opendkim.default.conf &&\
-    confd -onetime -backend env &&\
-    chown -R opendkim:opendkim /etc/opendkim &&\
-    chmod a+r /var/lib/unbound/root.key
+	confd -onetime -backend env &&\
+	chown -R opendkim:opendkim /etc/opendkim &&\
+	chmod a+r /var/lib/unbound/root.key
 
 # Ports
 EXPOSE 8891
--- root/run.sh
+++ root/run.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 
-# Set default environment
-export DKIM_DOMAIN="${DKIM_DOMAIN:-localdomain}"
-export DKIM_KEYFILE="${DKIM_KEYFILE:-/var/lib/opendkim/private.key}"
-export DKIM_SELECTOR="${DKIM_SELECTOR:-default}"
-export DKIM_INTERNALHOSTS="${DKIM_INTERNALHOSTS:-127.0.0.1}"
-
 # Generate configuration
 confd -onetime -backend env
 
 # Check permissions
 if [ -e "$DKIM_KEYFILE" ]; then
-    chmod 600 "$DKIM_KEYFILE" || true
-    chmod 700 `dirname "$DKIM_KEYFILE"` || true
+	chmod 600 "$DKIM_KEYFILE" || true
+	chmod 700 `dirname "$DKIM_KEYFILE"` || true
 else
-    echo "ERROR: key file '$DKIM_KEYFILE' is missing or not accessible"
-    exit 1
+	echo "ERROR: key file '$DKIM_KEYFILE' is missing or not accessible"
+	exit 1
 fi
 
 # Start OpenDKIM
